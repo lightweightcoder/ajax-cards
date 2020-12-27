879db537de2884aaa0b40b72e52456ca023f808a
aljt <alvinljt@gmail.com>
2020-12-27 17:09:31 +0800

    added login, logout, and register routes

--- routes.mjs
+++ routes.mjs
@@ -2,12 +2,24 @@ import db from './models/index.mjs';
 
 // import your controllers here
 import games from './controllers/games.mjs';
+import users from './controllers/users.mjs';
 
 export default function routes(app) {
+  // pass in db for all callbacks in controllers
   const GamesController = games(db);
+  const UsersController = users(db);
 
   // main page
   app.get('/', GamesController.index);
+
+  // log a user in
+  app.post('/login', UsersController.login);
+
+  // register a user
+  app.post('/register', UsersController.register);
+
+  // logout a user
+  app.delete('/logout', UsersController.logout);
 
   // create a new game
   app.post('/games', GamesController.create);
